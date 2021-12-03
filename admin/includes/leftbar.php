c784eb3b581b1851bf6d94a036b7656499ff8f43
Ljubiša Jovčić <91899896+LjubisaJ@users.noreply.github.com>
2021-12-03 15:30:37 +0100

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,26 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label"></li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i>Podaci</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i>Modeli</a>
+<ul>
+<li><a href="create-brand.php">Dodaj model</a></li>
+<li><a href="manage-brands.php">Izmeni model</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i>Automobili</a>
+					<ul>
+						<li><a href="post-avehical.php">Dodaj automobil</a></li>
+						<li><a href="manage-vehicles.php">Izmeni automobil</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i>Rezervacije automobila</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i>Upravljanje pitanjima</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i>Registrovani korisnici</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i>Izmeni kontakt informacije</a></li>
+
+			</ul>
+		</nav>
